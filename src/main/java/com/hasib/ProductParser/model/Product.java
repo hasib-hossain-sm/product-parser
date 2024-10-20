3e6389392a5a2fc48186ab8f0137daedd1f1eec5
hasib <hasibhossainsm@gmail.com>
2024-10-21 05:31:21 +0600

    fix @CreatedDate and @LastModifiedDate as this was not working as expected.

--- src/main/java/com/hasib/ProductParser/model/Product.java
+++ src/main/java/com/hasib/ProductParser/model/Product.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.util.Objects;
@@ -14,6 +15,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
