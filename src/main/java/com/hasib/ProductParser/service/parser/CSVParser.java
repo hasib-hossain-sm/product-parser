82ea87e6511ba33fe6d8de2f52592d992a586a61
hasib <hasibhossainsm@gmail.com>
2024-10-21 03:06:44 +0600

    Updated pom.xml: added opencsv dependency Updated CSVParser.java: developed the parseFile method Updated FileParser.java: added exception to the interface.

--- src/main/java/com/hasib/ProductParser/service/parser/CSVParser.java
+++ src/main/java/com/hasib/ProductParser/service/parser/CSVParser.java
@@ -2,16 +2,45 @@ package com.hasib.ProductParser.service.parser;
 
 
 import com.hasib.ProductParser.model.Product;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class CSVParser implements FileParser {
 
     @Override
-    public List<Product> parseFile(InputStream inputStream) {
-        return null;
+    public List<Product> parseFile(InputStream inputStream) throws IOException, CsvValidationException {
+        List<Product> products = new ArrayList<>();
+
+        try (CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream))) {
+            String[] values;
+            boolean isFirstRow = true;
+
+            while ((values = csvReader.readNext()) != null) {
+                if (isFirstRow) {
+                    isFirstRow = false;
+                    continue;
+                }
+
+                Product product = Product.builder()
+                        .sku(values[0])
+                        .title(values[1])
+                        .price(Double.parseDouble(values[2]))
+                        .quantity(Integer.parseInt(values[3]))
+                        .description(values[4])
+                        .build();
+
+                products.add(product);
+            }
+        }
+
+        return products;
     }
 }
