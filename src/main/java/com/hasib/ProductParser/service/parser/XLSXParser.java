19b03cf3ce62ea0aa194beacc798e9c217a87ce9
hasib <hasibhossainsm@gmail.com>
2024-10-21 02:58:56 +0600

    Updated pom.xml: added poi-ooxml dependency Updated XLSXParser.java: developed the parseFile method

--- src/main/java/com/hasib/ProductParser/service/parser/XLSXParser.java
+++ src/main/java/com/hasib/ProductParser/service/parser/XLSXParser.java
@@ -2,9 +2,16 @@ package com.hasib.ProductParser.service.parser;
 
 
 import com.hasib.ProductParser.model.Product;
+import org.apache.poi.EmptyFileException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -12,7 +19,31 @@ public class XLSXParser implements FileParser {
 
     @Override
     public List<Product> parseFile(InputStream inputStream) {
-        return null;
+        Workbook workbook;
+        try {
+            workbook = new XSSFWorkbook(inputStream);
+        } catch (IOException ex) {
+            throw new EmptyFileException();
+        }
+
+        Sheet sheet = workbook.getSheetAt(0);
+        List<Product> products = new ArrayList<>();
+
+        for (Row row : sheet) {
+            if (row.getRowNum() == 0) continue;
+
+            Product product = Product.builder()
+                    .sku(row.getCell(0).getStringCellValue())
+                    .title(row.getCell(1).getStringCellValue())
+                    .price(row.getCell(2).getNumericCellValue())
+                    .quantity((int) row.getCell(3).getNumericCellValue())
+                    .description(row.getCell(4).getStringCellValue())
+                    .build();
+
+            products.add(product);
+        }
+
+        return products;
 
     }
 }
