303822e124ab9f1f3324a594096c88fee42927b3
hasib <hasibhossainsm@gmail.com>
2024-10-20 23:08:20 +0600

    initial commit

---
+++ src/main/java/com/hasib/ProductParser/ProductParserApplication.java
@@ -0,0 +1,13 @@
+package com.hasib.ProductParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductParserApplication.class, args);
+	}
+
+}
